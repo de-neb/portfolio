9219dcea160d778d109570378bdd0a36ae190a99
Eden Baui <eedenbaui@gmail.com>
2021-05-11 23:49:56 +0800

    modify vue.conifg.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/web-portfolio-v2/" : "/",
+    process.env.NODE_ENV === "production" ? "/" : "/web-portfolio-v2/",
 };
